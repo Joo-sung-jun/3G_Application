bb978399c58c09c58d7598821ecf6003fdb8626a
Joo-sung-jun <wntjdwns6@naver.com>
2021-04-18 14:56:02 +0900

    add

--- app/src/main/java/com/example/a3gproject_java/Mypage.java
+++ app/src/main/java/com/example/a3gproject_java/Mypage.java
@@ -2,7 +2,10 @@ package com.example.a3gproject_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Mypage extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class Mypage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        Button bt_change = (Button) findViewById(R.id.bt_change);
+        bt_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Change.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button bt_logout = (Button) findViewById(R.id.bt_logout);
+        bt_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Logout.class);
+                startActivity(intent);
+            }
+        });
     }
 }
