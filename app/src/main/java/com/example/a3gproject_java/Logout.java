b239bd7a122eb228229d50498c32e8cdb7528285
KNS <skatjr98@naver.com>
2021-04-18 21:55:11 +0900

    카카오톡로그인
    백업파일 만들고 받아주세요

--- app/src/main/java/com/example/a3gproject_java/Logout.java
+++ app/src/main/java/com/example/a3gproject_java/Logout.java
@@ -1,14 +1,110 @@
 package com.example.a3gproject_java;
 
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.kakao.sdk.auth.LoginClient;
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.User;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+import kotlin.jvm.functions.Function2;
 
 public class Logout extends AppCompatActivity {
+    private static final String TAG = "Logout";
+
+    private View loginButton, logoutButton;
+    private TextView nickName;
+    private ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
+
+        loginButton = findViewById(R.id.login);
+        logoutButton = findViewById(R.id.logout);
+        nickName = findViewById(R.id.nickName);
+        profileImage = findViewById(R.id.profile);
+
+        Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
+            @Override
+            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
+                if(oAuthToken != null){
+
+                }
+                if(throwable != null){
+
+                }
+                updateKakaoLoginUi();
+                return null;
+            }
+        };
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(LoginClient.getInstance().isKakaoTalkLoginAvailable(Logout.this)) {
+                    LoginClient.getInstance().loginWithKakaoTalk(Logout.this, callback);
+
+                }
+                else{
+                    LoginClient.getInstance().loginWithKakaoAccount(Logout.this, callback);
+                }
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UserApiClient.getInstance().logout(new Function1<Throwable, Unit>() {
+                    @Override
+                    public Unit invoke(Throwable throwable) {
+                        updateKakaoLoginUi();
+                        return null;
+                    }
+                });
+            }
+        });
+
+        updateKakaoLoginUi();
     }
+
+    private void updateKakaoLoginUi(){
+        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+            @Override
+            public Unit invoke(User user, Throwable throwable) {
+                if(user != null){
+                    loginButton.setVisibility(View.GONE);
+                    logoutButton.setVisibility(View.VISIBLE);
+
+                    Log.i(TAG, "invoke: id=" + user.getId());
+                    Log.i(TAG, "invoke: email=" + user.getKakaoAccount().getEmail());
+                    Log.i(TAG, "invoke: nickname=" + user.getKakaoAccount().getProfile().getNickname());
+                    Log.i(TAG, "invoke: gender=" + user.getKakaoAccount().getGender());
+                    Log.i(TAG, "invoke: age=" + user.getKakaoAccount().getAgeRange());
+
+                    nickName.setText(user.getKakaoAccount().getProfile().getNickname());
+
+                } else {
+                    nickName.setText(null);
+
+                    loginButton.setVisibility(View.VISIBLE);
+                    logoutButton.setVisibility(View.GONE);
+
+                }
+                return null;
+            }
+        });
+    }
+
+
 }
