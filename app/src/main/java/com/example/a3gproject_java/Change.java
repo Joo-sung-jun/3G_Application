f598134aa6115934ab05293cbcd0cf786664c0c7
minju park <mlnjv016@gmail.com>
2021-04-18 17:37:30 +0900

    하단바 변경, 모든 액티비티에 하단바 전환 추가

--- app/src/main/java/com/example/a3gproject_java/Change.java
+++ app/src/main/java/com/example/a3gproject_java/Change.java
@@ -2,7 +2,10 @@ package com.example.a3gproject_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Change extends AppCompatActivity {
 
@@ -10,5 +13,45 @@ public class Change extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change);
+
+        //하단바 학점계산기 버튼 클릭 시 학점계산기 첫 화면으로 이동(홈화면)
+        ImageButton btn_calculator = (ImageButton) findViewById(R.id.btn_calculator);
+        btn_calculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Calculator.class);
+                startActivity(intent);
+            }
+        });
+
+        //하단바 체크리스트 버튼 클릭 시 체크리스트 첫 화면으로 이동
+        ImageButton btn_checklist = (ImageButton) findViewById(R.id.btn_checklist);
+        btn_checklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Checklist.class);
+                startActivity(intent);
+            }
+        });
+
+        //하단바 졸업요강 버튼 클릭 시 졸업요강 첫 화면을 이동
+        ImageButton btn_ppt = (ImageButton) findViewById(R.id.btn_ppt);
+        btn_ppt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Ppt.class);
+                startActivity(intent);
+            }
+        });
+
+        //하단바 마이페이지 버튼 클릭 시 마이페이지 첫 화면으로 이
+        ImageButton btn_account = (ImageButton) findViewById(R.id.btn_mypage);
+        btn_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Mypage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/a3gproject_java/Mypage.java
+++ app/src/main/java/com/example/a3gproject_java/Mypage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Mypage extends AppCompatActivity {
 
@@ -13,6 +14,46 @@ public class Mypage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        //하단바 학점계산기 버튼 클릭 시 학점계산기 첫 화면으로 이동(홈화면)
+        ImageButton btn_calculator = (ImageButton) findViewById(R.id.btn_calculator);
+        btn_calculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Calculator.class);
+                startActivity(intent);
+            }
+        });
+
+        //하단바 체크리스트 버튼 클릭 시 체크리스트 첫 화면으로 이동
+        ImageButton btn_checklist = (ImageButton) findViewById(R.id.btn_checklist);
+        btn_checklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Checklist.class);
+                startActivity(intent);
+            }
+        });
+
+        //하단바 졸업요강 버튼 클릭 시 졸업요강 첫 화면을 이동
+        ImageButton btn_ppt = (ImageButton) findViewById(R.id.btn_ppt);
+        btn_ppt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Ppt.class);
+                startActivity(intent);
+            }
+        });
+
+        //하단바 마이페이지 버튼 클릭 시 마이페이지 첫 화면으로 이
+        ImageButton btn_account = (ImageButton) findViewById(R.id.btn_mypage);
+        btn_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Mypage.class);
+                startActivity(intent);
+            }
+        });
 
         Button bt_change = (Button) findViewById(R.id.bt_change);
         bt_change.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +73,6 @@ public class Mypage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
